15bff65411e9adb1db1758574b1e1f1472ce535c
Yury <matatov1989@gmail.com>
2023-11-01 14:28:49 +0200

    prepating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Places({ title, places, fallbackText, onSelectPlace }) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="places-category">
+      <h2>{title}</h2>
+      {places.length === 0 && <p className='fallback-text'>{fallbackText}</p>}
+      {places.length > 0 && (
+        <ul className="places">
+          {places.map((place) => (
+            <li key={place.id} className="place-item">
+              <button onClick={() => onSelectPlace(place.id)}>
+                <img src={place.image.src} alt={place.image.alt} />
+                <h3>{place.title}</h3>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
-
-export default App
